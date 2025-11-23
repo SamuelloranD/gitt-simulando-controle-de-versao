ba079db69a263c11334ed9deb6981d304013658e
iarlon <iarlonpvh@gmail.com>
2025-11-23 16:53:47 -0300

    fazendo adição

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class Main {
+    public static Double Adicao(double a, double b){
+        return a + b;
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Grupo 2\n" +
                 "Desafio de Merge: “Simulando o controle de versão”\n ");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        double resultado = Adicao(a, b);
+        System.out.println("Resultado: " + resultado);
     }
 }
