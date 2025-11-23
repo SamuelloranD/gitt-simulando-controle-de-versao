4e51832bc3b92592bc30249eafa58248badb947f
Iarlon Soares <163212148+Iarlon@users.noreply.github.com>
2025-11-23 17:30:35 -0300

    Revert "fazendo adição"

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,14 @@
-import java.util.Scanner;
 public class Main {
-    public static Double Adicao(double a, double b){
-        return a + b;
-    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         System.out.println("Grupo 2\n" +
                 "Desafio de Merge: “Simulando o controle de versão”\n ");
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-
-        double resultadoAdicao = Adicao(a, b);
-        System.out.println("Resultado: " + resultado);
 
         System.out.println("Operações:");
 
         int numero1 = 10;
         int numero2 = 4;
-        int resultadoSubtracao = numero1 - numero2;
+        int resultado = numero1 - numero2;
 
-        System.out.println("Resultado da subtração: " + resultadoSubtracao);
-        System.out.println("Resultado da adição: " + resultadoAdicao);
+        System.out.println("Resultado da subtração: " + resultado);
     }
 }
