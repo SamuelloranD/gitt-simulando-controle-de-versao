1b9c72719343f42dac68bab1055b525ee15c40f0
Samuel Lorand <samuel.lorand@hotmail.com>
2025-11-23 15:05:02 -0300

    Inicial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Grupo 2\n" +
+                "Desafio de Merge: “Simulando o controle de versão”\n ");
+    }
+}
