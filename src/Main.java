3b951b62e64cd0a43810fedbb31d31f1bb9aad77
Samuel Lorand <samuel.lorand@hotmail.com>
2025-11-23 16:49:29 -0300

    Revert "Merge pull request #2 from SamuelloranD/develop"
    This reverts commit 6fa893aadd6dfc21920ae714f297e9b5912b10d3, reversing
    changes made to 1b9c72719343f42dac68bab1055b525ee15c40f0.

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Grupo 2\n" +
                 "Desafio de Merge: “Simulando o controle de versão”\n ");
-
-        System.out.println("Operações:");
-
-        int numero1 = 10;
-        int numero2 = 4;
-        int resultado = numero1 - numero2;
-
-        System.out.println("Resultado da subtração: " + resultado);
     }
 }
